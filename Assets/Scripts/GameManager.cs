63561fbb761c484055caa5a208c8fcef15579c13
Pigletnaja <loukmoo12@gmail.com>
2026-08-24 22:21:16 +0700

    Score Text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -22,6 +23,15 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private float xInput=0f;
 
+    [SerializeField]
+    private GameObject ballLine;
+
+    [SerializeField]
+    private GameObject cam;
+
+    [SerializeField]
+    private TMP_Text notiText;
+
     public static GameManager instance;
     void Awake()
     {
@@ -30,6 +40,8 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        CameraBehindCueBall();  
+
         SetBall(BallColor.Red, 1);
         SetBall(BallColor.Yellow, 2);
         SetBall(BallColor.Green, 3);
@@ -48,11 +60,14 @@ public class GameManager : MonoBehaviour
         if(Keyboard.current.spaceKey.wasPressedThisFrame)
             ShootBall();
         if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
-            xInput = -0.1f;
+            xInput = -0.05f;
         else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
-            xInput = 0.1f;
+            xInput = 0.05f;
         else
             xInput = 0f;
+
+        if (Keyboard.current.backspaceKey.wasPressedThisFrame)
+            StopBall();
     }
     private void SetBall(BallColor col,int i)
     {
@@ -68,11 +83,43 @@ public class GameManager : MonoBehaviour
     {
         Rigidbody rd = cueBall.GetComponent<Rigidbody>();
         rd.AddRelativeForce(Vector3.forward * 50, ForceMode.Impulse);
+
+        ballLine.SetActive(false);
+        cam.transform.parent = null;
+        cam.transform.position = new Vector3(0f, 30f, -42f);
+        cam.transform.eulerAngles = new Vector3(45f,0f,0f);
     }
     private void RotateBall()
     {
         if (cueBall != null)
             cueBall.transform.Rotate(new Vector3(0f, xInput, 0f));
     }
+    private void StopBall()
+    {
+        Rigidbody rb = cueBall.GetComponent<Rigidbody>();
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        cueBall.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+
+        ballLine.SetActive(true);
+        CameraBehindCueBall();
+    }
+
+    private void CameraBehindCueBall()
+    {
+        cam.transform.parent = cueBall.transform;
+        cam.transform.position = cueBall.transform.position + new Vector3(0f,7f,-15f);
+        cam.transform.eulerAngles = new Vector3(30f,0f,0f);
+    }
+    public void ShowScoreText(int n)
+    {
+        playerScore += n;
+        notiText.text = $"Ball Point:{n}\nTotal Score:{playerScore}";
+    }
+    public void ShowString(string s)
+    {
+        notiText.text = s;
+    }
+
 
 }
---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+   private void OnTriggerEnter(Collider other)
+    {
+        Ball b = other.GetComponent<Ball>();
+        if (b != null )
+        {
+            if (b.Point == 0)
+            {
+                GameManager.instance.ShowString($"White Ball drop!!!\nYou lose!!!");
+            }
+            else
+            {
+                GameManager.instance.ShowScoreText(b.Point);
+            }
+            GameManager.instance.ShowScoreText(b.Point);
+            Destroy(b.gameObject);
+        }
+
+
+    }
+}
